7b9955df0f4102bb6b392e7af125d4e4b0aae0f5
Bailey Hill <bhill1@mail.sfsu.edu>
2020-02-05 18:01:53 -0800

    created two js files

---
+++ components/app.js
@@ -0,0 +1,24 @@
+class App {
+  constructor() {
+    this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess.bind(this);
+  }
+  handleGetGradesError(err){
+    console.error(err)
+  }
+  handleGetGradesSuccess(grades){
+    console.log(grades)
+  }
+
+  getGrades(){
+    $.get({
+      url: "http://sgt.lfzprototypes.com/api/grades",
+      headers: { "X-Access-Token": "zN4n1ZW3" },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError,
+    })
+  }
+  start(){
+    this.getGrades();
+  }
+}
