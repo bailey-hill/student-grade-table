a91348c378c367ba1ab580869c396839314b7af3
Bailey Hill <bhill1@mail.sfsu.edu>
2020-02-06 10:29:23 -0800

    updatedGrades modified to empty tbody

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,8 @@ class GradeTable {
     this.tableElement;
   }
   updateGrades(grades){
+    var tableBody = document.queryselector(".tableBody")
+    tableBody.empty();
     console.log(grades);
   }
 }
